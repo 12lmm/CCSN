6bb69763b38db3cd20e36756cffe68d051a87f1f
12lmm <168911161+12lmm@users.noreply.github.com>
2025-04-16 11:41:45 +0800

    Update train.sh
    Replace * with your own dataset storage location

--- train.sh
+++ train.sh
@@ -20,32 +20,28 @@ WORKERS=${11}
 PORT=${12}
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo $DIR          # 打印DIR
+echo $DIR        
 
 # datasets
 echo "Checking $DATASET"
 NUM_CLASSES=0
 
-#if [ "$DATASET" = "THFOOD50" ]; then  # 数据集可能有问题，导致代码总是出错
-#  DATA_DIR=/home/liumengmeng/HW3/VGG/data/THFOOD50/
-
-
 if [ "$DATASET" = "food11" ]; then
-  DATA_DIR=/home/liumengmeng/HW3/VGG/data/food11/
+  DATA_DIR=*/data/food11/
   if [ ! -d $DATA_DIR ]; then
     echo "not found $DATA_DIR"
     exit
   fi
   NUM_CLASSES=11
 elif [ "$DATASET" = "ETHZ_Food101" ]; then
-  DATA_DIR=/home/liumengmeng/HW3/VGG/data/ETHZ_Food101/
+  DATA_DIR=*/data/ETHZ_Food101/
   if [ ! -d $DATA_DIR ]; then
     echo "not found $DATA_DIR"
     exit
   fi
   NUM_CLASSES=101
 elif [ "$DATASET" = "food_new" ]; then
-  DATA_DIR=/home/liumengmeng/HW3/VGG/data/food_new/
+  DATA_DIR=*/data/food_new/
   if [ ! -d $DATA_DIR ]; then
     echo "not found $DATA_DIR"
     exit
@@ -53,44 +49,12 @@ elif [ "$DATASET" = "food_new" ]; then
   NUM_CLASSES=20
 
 elif [ "$DATASET" = "CAFD" ]; then
-  DATA_DIR=/home/liumengmeng/HW3/VGG/data/CAFD/
+  DATA_DIR=*/data/CAFD/
   if [ ! -d $DATA_DIR ]; then
     echo "not found $DATA_DIR"
     exit
   fi
   NUM_CLASSES=42
-
-elif [ "$DATASET" = "THFOOD50" ]; then
-DATA_DIR=/home/liumengmeng/HW3/VGG/data/THFOOD50/
-if [ ! -d $DATA_DIR ]; then
-  echo "not found $DATA_DIR"
-  exit
-fi
-NUM_CLASSES=50
-
-elif [ "$DATASET" = "UECFOOD-100" ]; then
-DATA_DIR=/home/liumengmeng/HW3/VGG/data/UECFOOD-100/
-if [ ! -d $DATA_DIR ]; then
-  echo "not found $DATA_DIR"
-  exit
-fi
-NUM_CLASSES=100
-
-elif [ "$DATASET" = "UECFOOD-256" ]; then
-DATA_DIR=/home/liumengmeng/HW3/VGG/data/UECFOOD-256/
-if [ ! -d $DATA_DIR ]; then
-  echo "not found $DATA_DIR"
-  exit
-fi
-NUM_CLASSES=256
-
-elif [ "$DATASET" = "MAFood121" ]; then
-DATA_DIR=/home/liumengmeng/HW3/VGG/data/MAFood121/
-if [ ! -d $DATA_DIR ]; then
-  echo "not found $DATA_DIR"
-  exit
-fi
-NUM_CLASSES=121
 
 else
   echo "Unknown $DATASET"
@@ -115,7 +79,7 @@ echo "RM_OLD_IF_EXIST: $RM_OLD_IF_EXIST"
 echo "RESUM_OLD_IF_EXIST: $RESUM_OLD_IF_EXIST"
 if [ -d $EXPERIMENT_DIR ]; then
   echo "$EXPERIMENT_DIR --- Already exists"
-  if [ $RM_OLD_IF_EXIST -gt 0 ]; then    # RM_OLD_IF_EXIST > 0,询问是否删除已存在的实验目录
+  if [ $RM_OLD_IF_EXIST -gt 0 ]; then   
     while true; do
         read -p "Are you sure to delete this result directory? " yn
         case $yn in
@@ -125,7 +89,7 @@ if [ -d $EXPERIMENT_DIR ]; then
         esac
     done
   else
-    if [ $RESUM_OLD_IF_EXIST -gt 0 ]; then   #  RESUM_OLD_IF_EXIST > 0,恢复训练
+    if [ $RESUM_OLD_IF_EXIST -gt 0 ]; then   
       echo "Auto-resume"
     else
       echo "Skip"
